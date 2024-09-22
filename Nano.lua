5e30b869eeb6be48e19defa07b1e21d3cd2ec96e
Even-dev <samuca1234.oficial@gmail.com>
2024-09-21 22:21:35 -0300

    Update Nano.lua

--- Nano.lua
+++ Nano.lua
@@ -1,5 +1,5 @@
-
 local commands = {}
+local variables = {}
 
 -- Função para criar um arquivo HTML
 function criarArquivoHTML()
@@ -9,76 +9,24 @@ function criarArquivoHTML()
     print("Arquivo HTML criado com sucesso!")
 end
 
-function ramo(n)
-  n = math.random(1, 230)
-  print(n)
-end
-
-function n()
-  inn = tonumber(io.read())
-  
-  
+function ramo()
+    local n = math.random(1, 230)
+    print(n)
 end
 
 function ane()
-  
-  local data_hora_atual = os.date("*t")
-  print(data_hora_atual.year)
-  
+    local data_hora_atual = os.date("*t")
+    print(data_hora_atual.year)
 end
-
 
 function date()
-  local data_hora_atual = os.date("*t")
-print("Dia: " .. data_hora_atual.day)
-print("Mês: " .. data_hora_atual.month)
-print("Ano: " .. data_hora_atual.year)
-print("Hora: " .. data_hora_atual.hour)
-print("Minuto: " .. data_hora_atual.min)
-print("Segundo: " .. data_hora_atual.sec)
-
-end
-
-function dia()
-   local data_hora_atual = os.date("*t")
-   print("Dia: " .. data_hora_atual.day)
-   
-end
-
-function mes()
-   local data_hora_atual = os.date("*t")
-   print("Mês: " .. data_hora_atual.month)
-   
-end
-
-function hora()
-  local data_hora_atual = os.date("*t")
-  print("Hora: ".. data_hora_atual.hour)
-end
-
-function minuto()
-  local data_hora_atual = os.date("*t")
-  print("Minuto: " .. data_hora_atual.min)
-end
-
-function segundo()
-  
-  local data_hora_atual = os.date("*t")
-  print("Segundo: " .. data_hora_atual.sec)
-end
-
-function inp()
-  local inpu = io.read()
-end
-  function tex_inp(inpu)
-    print(inpu)
-  end
- 
-
-function inp_nun()
-  local inp = tonumber(io.read())
-  
-  
+    local data_hora_atual = os.date("*t")
+    print("Dia: " .. data_hora_atual.day)
+    print("Mês: " .. data_hora_atual.month)
+    print("Ano: " .. data_hora_atual.year)
+    print("Hora: " .. data_hora_atual.hour)
+    print("Minuto: " .. data_hora_atual.min)
+    print("Segundo: " .. data_hora_atual.sec)
 end
 
 function criarArquivoCSS()
@@ -88,43 +36,62 @@ function criarArquivoCSS()
     print("Arquivo CSS criado com sucesso!")
 end
 
--- Função para alterar a cor de fundo para azul
 function azul()
-    local file = io.open("index.css", "w")file:write("body { background-color: blue; }") 
+    local file = io.open("index.css", "w")
+    file:write("body { background-color: blue; }")
     file:close()
     print("Cor do fundo alterada para Azul")
 end
 
--- Função para mudar a cor do fundo para verde
 function verde()
-    local file = io.open("index.css", "w") 
-    file:write("body { background-color: green; }") 
+    local file = io.open("index.css", "w")
+    file:write("body { background-color: green; }")
     file:close()
-    print("Cor do fundo alterada para Verde") 
+    print("Cor do fundo alterada para Verde")
 end
-
-function raiz()
-  local input = tonumber(io.read())
-    local raiz = input * input
-      print(raiz)
-  
-  
-  
-end
-
-
 
 function preto()
-  local file = io.open("index.css", "w") 
-    file:write("body { background-color: black; }") 
+    local file = io.open("index.css", "w")
+    file:write("body { background-color: black; }")
     file:close()
-    print("Cor do fundo alterada para preto") 
+    print("Cor do fundo alterada para Preto")
 end
 
+function definirVariavel(input)
+    local nome, valor = input:match("var%s+(%w+)%s*=%s*(.+)")
+    if nome and valor then
+        variables[nome] = valor
+        
+    else
+        print("Erro: Formato inválido. Use 'var nome=valor'.")
+    end
+end
 
+function mostrarVariavel(input)
+    local nome = input:match("tex%((%w+)%)")
+    if nome and variables[nome] then
+        print(variables[nome])
+    else
+        print("Erro: Variável não encontrada ou formato inválido.")
+    end
+end
 
-function n()
-  print("ㅤ")
+function executarFor(input)
+    local repeticoes, codigo = input:match("for%s*%((%d+)%)%s*{(.+)}")
+    if repeticoes and codigo then
+        repeticoes = tonumber(repeticoes)
+        for i = 1, repeticoes do
+            -- Remove espaços desnecessários e imprime o código a ser repetido
+            local comando = codigo:gsub("%s+", "")
+            if comando:match("tex%((%w+)%)") then
+                mostrarVariavel(comando)
+            else
+                print("Comando desconhecido: " .. comando)
+            end
+        end
+    else
+        print("Erro: Formato inválido. Use 'for(número) { código }'.")
+    end
 end
 
 -- Loop para verificar a entrada do usuário e armazenar os comandos
@@ -133,54 +100,31 @@ while true do
     if input == "sair" then
         print("Saindo do programa.")
         break
-    elseif input == "#" then
-        for _, command in ipairs(commands) do
-            if command == "/*" then
-                criarArquivoHTML()
-            elseif command == "//" then
-                criarArquivoCSS()
-            elseif command == "color f = azul" then
-                azul()
-            elseif command == "color f = verde" then
-                verde()
-              elseif command == "color f = preto" then
-                preto()
-              elseif command == "new date = ano" then
-                ane()
-                elseif command == "new date = hora" then
-                hora()
-                elseif command == "new date = dia" then
-                  dia()
-                elseif command == "new date = mês" then
-                  mes()
-                elseif command == "new date = minuto" then
-                minuto()
-                 elseif command == "new date = date" then
-                date() 
-              elseif command == "/n" then
-                n()
-             elseif command == "radow()" then
-                ramo()
-             elseif command  == "create_input(n)" then
-               n()
-              elseif command == "new date = segundos" then
-                segundo() 
-              elseif command == "::raiz"  then
-                raiz()
-            elseif command == "text" then
-              print(" erro not is 'text' is Tex ")
-            elseif command == "create_input" then
-              inp()
-              
-              
-            elseif string.sub(command, 1, 4) == "tex(" or "TEX("then
-                local texto = string.match(command, "%((.+)%)")
-                if texto then
-                    print(texto) -- Exibe o texto entre parênteses
-                end
-            end
-        end
+    elseif input:sub(1, 3) == "var" then
+        definirVariavel(input)
+    elseif input:sub(1, 3) == "tex" then
+        mostrarVariavel(input)
+    elseif input:sub(1, 3) == "for" then
+        executarFor(input)
     else
-        table.insert(commands, input) 
+        table.insert(commands, input)
+        -- Adicionando comandos já existentes
+        if input == "/*" then
+            criarArquivoHTML()
+        elseif input == "//" then
+            criarArquivoCSS()
+        elseif input == "color f = azul" then
+            azul()
+        elseif input == "color f = verde" then
+            verde()
+        elseif input == "color f = preto" then
+            preto()
+        elseif input == "new date = ano" then
+            ane()
+        elseif input == "new date = date" then
+            date()
+        elseif input == "radow()" then
+            ramo()
+        end
     end
-end 
+end
